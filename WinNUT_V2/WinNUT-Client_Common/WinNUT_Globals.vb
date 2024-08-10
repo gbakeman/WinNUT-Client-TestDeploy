31fc636d81a52821349ba078f16e1eefc1348e1a
gbakeman <gbakeman@live.com>
2024-08-10 13:08:46 -0400

    Common library Updater changes - Modified global variables to avoid implicit definitions - Added update controller to global variables - Updated OctoKit NuGet library to latest version - Removing update provider classes and interface in favor of single defined class with multiple event argument classes

--- WinNUT_V2/WinNUT-Client_Common/WinNUT_Globals.vb
+++ WinNUT_V2/WinNUT-Client_Common/WinNUT_Globals.vb
@@ -1,23 +1,16 @@
-﻿' WinNUT-Client is a NUT windows client for monitoring your ups hooked up to your favorite linux server.
-' Copyright (C) 2019-2021 Gawindx (Decaux Nicolas)
-'
-' This program is free software: you can redistribute it and/or modify it under the terms of the
-' GNU General Public License as published by the Free Software Foundation, either version 3 of the
-' License, or any later version.
-'
-' This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY
-
-Public Module WinNUT_Globals
+﻿Public Module WinNUT_Globals
 
 #Region "Constants/Shareds"
 
-    Public ReadOnly ProgramName = My.Application.Info.ProductName
-    Public ReadOnly ProgramVersion = My.Application.Info.Version.ToString()
-    Public ReadOnly ShortProgramVersion = ProgramVersion.Substring(0, ProgramVersion.IndexOf(".", ProgramVersion.IndexOf(".") + 1))
-    Public ReadOnly GitHubURL = My.Application.Info.Trademark
-    Public ReadOnly Copyright = My.Application.Info.Copyright
+    Public ReadOnly ProgramName As String = My.Application.Info.ProductName
+    Public ReadOnly ProgramVersion As String = My.Application.Info.Version.ToString()
+    Public ReadOnly ShortProgramVersion As String =
+        ProgramVersion.Substring(0, ProgramVersion.IndexOf(".", ProgramVersion.IndexOf(".") + 1))
+    Public ReadOnly GitHubURL As String = My.Application.Info.Trademark
+    Public ReadOnly Copyright As String = My.Application.Info.Copyright
 
     Public WithEvents LogFile As Logger = New Logger(LogLvl.LOG_DEBUG)
+    Public WithEvents UpdateController As New Updater.UpdateUtil
     Public StrLog As New List(Of String)
 
 #End Region
